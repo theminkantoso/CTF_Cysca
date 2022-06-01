2008729d704de75d4b481c1e42cc8ae3506eff7b
joseleii <Phambaodu25072001@gmail.com>
2022-06-01 23:02:06 +0700

    chop suey

---
+++ Crypto/Chop Suey/README.md
@@ -0,0 +1,7 @@
+Chạy lệnh sau: <br/>
+<code>python script.py</code><br/>
+Kết quả có 2 trường là <code>A's response</code> và <code>Secret response</code><br/>
+Ta lấy <code>A's response</code> XOR với <code>41414141414141414141414141414141414141414141414141</code><br/>
+Lấy KQ phép XOR này XOR với <code>Secret response</code> đế thêm '000000' vào để đủ số ký tự<br/>
+Lấy KQ phép XOR thứ hai này chuyển về dạng ASCII<br/>
+Cờ là <code>FriendNoticeBelfast525</code>
---
+++ Crypto/Chop Suey/script.py
@@ -0,0 +1,27 @@
+import socket
+clisock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clisock.connect(("192.168.100.210",1337))
+respdict = dict()
+#Recv Key Reset message and Banner
+print clisock.recv(1024)
+print clisock.recv(1024)
+while len(respdict) < 10000:
+	#Send encrypt
+	clisock.send("E,"+"A"*25+"\n")
+	resp = clisock.recv(1024).strip()
+	parts = resp.split(":")
+	respdict[parts[0]] = parts[1]
+	#print parts
+	if len(respdict) % 200 == 0:
+		print str(len(respdict))+"/10000"
+#Dump the service secret
+clisock.send("D\n")
+secretdata = clisock.recv(1024).strip()
+parts = secretdata.split(":")
+#Look for the IV in the dict
+if parts[0] in respdict:
+	print "Found secret iv in dict. Dumping responses"
+	print " A's response:",respdict[parts[0]]
+	print "Secret response:",parts[1]
+else:
+	print "Didn't find secret iv in dict. Try again"
